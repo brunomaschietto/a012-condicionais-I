f6cc60eef89e2127c309b1256cd1488c39e73ad5
brunomaschietto <bruno4_10@hotmail.com>
2022-09-06 15:40:49 -0300

    Exercicios resolvidos aula 12

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,27 @@
-let idade;
-let isMaiorIdade;
+let idade = Number(prompt('Digite sua idade:'))
+let isMaiorIdade = confirm('Você terminou o ensino médio?')
 let isCursandoOutraFaculdade;
 
-if(){
-
+if(idade >= 18){
+    console.log('Você é maior de idade')
 }else{
-	
+	console.log('Você é menor de idade')
+}
+
+if (isMaiorIdade === true){
+    console.log('Você terminou o ensino médio')
+    isCursandoOutraFaculdade = confirm('Você está cursando alguma faculdade?')
+    if (isCursandoOutraFaculdade) {
+        console.log('Parabéns, você está cursando uma faculdade')
+    } else{
+        console.log('Não cursa nenhuma faculdade')
+    }
+} else {
+    console.log('Você não terminou o ensino médio')
+}
+
+if (idade >= 18 && isMaiorIdade === true && isCursandoOutraFaculdade === false){
+    console.log('Afirmação verdadeira')
+} else {
+    console.log('Afirmação falsa')
 }
